47d93cae55aa2f2adbd4446b5cfb2607b53e6ec2
barrena <barrena@172.22.58.105>
2026-04-09 12:32:20 +0200

    Creado el repositorio y la aplicacion de la practica de Java

---
+++ PL2_DesarrolloSoftware/src/main/java/poo/pl2_desarrollosoftware/PL2_DesarrolloSoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pl2_desarrollosoftware;
+
+/**
+ *
+ * @author barrena
+ */
+public class PL2_DesarrolloSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
